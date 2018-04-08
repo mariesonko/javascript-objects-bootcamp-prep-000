527b786b90b8d14301151043d9cae7e61dcb7043
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-08 15:41:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artistName: "songTitles"}
 
 function updatePlaylist (playlist, artistName, songTitles ){
-  return Object.assign({}, playlist, {[artistName]: "songTitles"})
+  return Object.assign(playlist, {[artistName]: "songTitles"})
 }
 
 function removeFromPlaylist (playlist, artistName){
