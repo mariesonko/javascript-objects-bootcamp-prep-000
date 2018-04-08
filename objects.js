f3560241b97fe459b08f97b7b0edf64da4001fff
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-08 16:17:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,1 @@
-var playlist = {artistName: "songTitles"}
 
-function updatePlaylist (playlist, artistName, songTitles ){
-  return Object.assign(playlist, {[artistName]: "songTitles"})
-}
-
-function removeFromPlaylist (playlist, artistName){
-  delete playlist[artistName]
-  return playlist
-
-}
